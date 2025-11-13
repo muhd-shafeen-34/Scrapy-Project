7b8f6f55fc1085bf97713e388f4f576aa2e60526
Shafeeney <shafeenshaznfc@gmail.com>
2025-11-13 14:04:13 +0530

    scrapped all the details of first agent and working fine

--- scrapy_project/spiders/agents.py
+++ scrapy_project/spiders/agents.py
@@ -29,8 +29,84 @@ class AgentSpider(scrapy.Spider):
 
     #this function will parse the agent details
     def parse_details(self,response):
+
+        #Getting full name
+
         names = response.css("h1.profileCard-name.textIntent-headline1::text").get().split()
-        print("first agent name = ",names)
+        #cleaning the name to yield first name middle name last name
+        first , middle , last = "","",""
+        if len(names) == 1:
+            first = names[0]
+        elif len(names) == 2:      
+            first,last = names
+        elif len(names) >=3:
+            first,middle,last = names[0], names[1], names[-1]
+        
+        profile = response.url
+
+        image = response.css("img.profile-image::attr(src)").get()
+
+        about = response.css("div.profile-body::text").getall()
+
+        #cleaning about
+
+        about_full = " ".join(about).replace("\n","")
+
+        # Getting social links and its corresponding apps to make it as a dict
+
+        social_links = response.css("div.profile-experience a::attr(href)").getall()
+        social_text = response.css("div.profile-experience b::text").getall()
+        social = dict(zip(social_text,social_links))
+        
+        #getting email
+
+        email = response.css("a.profileCard-email::text").get()
+
+        #getting title
+
+        title = response.css("div.titleCard.textIntent-body::text").get()
+
+        #getting numbers
+
+        numbers = response.css("div.phoneCard.textIntent-body a::text").getall()
+
+        #cleaning numbers to yield
+
+        phonenumbers = []
+        officenumbers = []
+        for i in numbers:
+            if ": " in i:
+                number = i.split(": ")[1]
+                phonenumbers.append(number)
+                if i.startswith("O: "):
+                    number2= i.split(": ")[1]
+                    officenumbers.append(number2)
+
+
+        yield {
+            "profile_url" : profile,
+            "first_name": first,
+            "middle_name": middle,
+            "last_name" : last,
+            "image_url": image,
+            "desciption": about_full,
+            "social": social,
+            "email": email,
+            "title": title,
+            "agent_phone_numbers": phonenumbers,
+            "office_phone_numbers": officenumbers,
+
+
+
+        }
+
+
+
+
+
+
+
+        
 
 
 
