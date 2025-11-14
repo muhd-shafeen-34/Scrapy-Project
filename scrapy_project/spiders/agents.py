521e53fcfdc522c74e3db751dac57caaf6d240bd
Shafeeney <shafeenshaznfc@gmail.com>
2025-11-14 15:39:48 +0530

    testd the workflow on all the pages in the website

--- scrapy_project/spiders/agents.py
+++ scrapy_project/spiders/agents.py
@@ -6,6 +6,7 @@ class AgentSpider(scrapy.Spider):
     allowed_domains = ["compass.com"]
 
     start_urls = ["https://www.compass.com/agents/locations/district-of-columbia-dc/30522/"]
+
     current_page = 1 # instance variable
 
     #function to crawl through the agent cards
@@ -26,7 +27,13 @@ class AgentSpider(scrapy.Spider):
         
             if link:
                 print("following first card link",link)
-                yield response.follow(link, callback=self.parse_details)
+                print("printed details of the links")
+                # yield response.follow(link, callback=self.parse_details)
+
+
+        base_url = response.url.split('?')[0]  # Remove existing query parameters
+        next_page = f"{base_url}?page={self.current_page}"
+        yield response.follow(next_page,callback = self.parse)
         self.current_page += 1
 
 
@@ -40,109 +47,109 @@ class AgentSpider(scrapy.Spider):
     
 
     # #this function will parse the agent details
-    def parse_details(self,response):
+    # def parse_details(self,response):
 
-        #Getting full name
+    #     #Getting full name
 
-        names = response.css("h1.profileCard-name.textIntent-headline1::text").get().split()
-        #cleaning the name to yield first name middle name last name
-        first , middle , last = "","",""
-        if len(names) == 1:
-            first = names[0]
-        elif len(names) == 2:      
-            first,last = names
-        elif len(names) >=3:
-            first,middle,last = names[0], names[1], names[-1]
+    #     names = response.css("h1.profileCard-name.textIntent-headline1::text").get().split()
+    #     #cleaning the name to yield first name middle name last name
+    #     first , middle , last = "","",""
+    #     if len(names) == 1:
+    #         first = names[0]
+    #     elif len(names) == 2:      
+    #         first,last = names
+    #     elif len(names) >=3:
+    #         first,middle,last = names[0], names[1], names[-1]
         
-        profile = response.url
+    #     profile = response.url
 
-        image = response.css("img.profile-image::attr(src)").get()
+    #     image = response.css("img.profile-image::attr(src)").get()
 
-        #getting about
-        # about = response.css("div.profile-body::text").getall()
+    #     #getting about
+    #     # about = response.css("div.profile-body::text").getall()
 
-        about = response.css("div.profile-about")
-        full_texts = about.css("::text").getall()
+    #     about = response.css("div.profile-about")
+    #     full_texts = about.css("::text").getall()
 
-        #cleaning about
+    #     #cleaning about
 
-        cleaned_texts = []
-        for text in full_texts:
-            strip = text.strip()
-            if strip:
-                cleaned_texts.append(strip)
-        description = "".join(cleaned_texts).replace("\n","")
-
-
-        # Getting social links  to make it as a dict
-
-        social_links = response.css("div.profile-experience a::attr(href)").getall()
-
-        #cleaning social links
-        cleaned_links = []
-        for i in social_links:
-            if i not in cleaned_links:
-                cleaned_links.append(i)
+    #     cleaned_texts = []
+    #     for text in full_texts:
+    #         strip = text.strip()
+    #         if strip:
+    #             cleaned_texts.append(strip)
+    #     description = "".join(cleaned_texts).replace("\n","")
 
 
-            #excluded concierge social becuase the example in the task has excluded the concierge social
-        platforms = ['facebook', 'instagram', 'linkedin', 'twitter','youtube','tiktok','pintrest']
+    #     # Getting social links  to make it as a dict
+
+    #     social_links = response.css("div.profile-experience a::attr(href)").getall()
+
+    #     #cleaning social links
+    #     cleaned_links = []
+    #     for i in social_links:
+    #         if i not in cleaned_links:
+    #             cleaned_links.append(i)
+
+
+    #         #excluded concierge social becuase the example in the task has excluded the concierge social
+    #     platforms = ['facebook', 'instagram', 'linkedin', 'twitter','youtube','tiktok','pintrest']
         
 
-        # here store the link and its corresponding social media platforms
-        social_links = {}
+    #     # here store the link and its corresponding social media platforms
+    #     social_links = {}
 
-    #this section is used to find the platform from the link using nester for loops
+    # #this section is used to find the platform from the link using nester for loops
     
-        for link in cleaned_links:
-            lower_link = link.lower()
-            for platform in platforms:
-                if platform in lower_link:
-                    if platform not in social_links:
-                        social_links[platform] = link
-                    break
+    #     for link in cleaned_links:
+    #         lower_link = link.lower()
+    #         for platform in platforms:
+    #             if platform in lower_link:
+    #                 if platform not in social_links:
+    #                     social_links[platform] = link
+    #                 break
 
-        #getting emailc
+    #     #getting emailc
 
-        email = response.css("a.profileCard-email::text").get()
+    #     email = response.css("a.profileCard-email::text").get()
 
-        #getting title
+    #     #getting title
 
-        title = response.css("div.titleCard.textIntent-body::text").get()
+    #     title = response.css("div.titleCard.textIntent-body::text").get()
 
-        #getting numbers
+    #     #getting numbers
 
-        numbers = response.css("div.phoneCard.textIntent-body a::text").getall()
+    #     numbers = response.css("div.phoneCard.textIntent-body a::text").getall()
 
-        #cleaning numbers to yield
+    #     #cleaning numbers to yield
 
-        phonenumbers = []
-        officenumbers = []
-        for i in numbers:
-            if ": " in i:
-                number = i.split(": ")[1]
-                phonenumbers.append(number)
-                if i.startswith("O: "):
-                    number2= i.split(": ")[1]
-                    officenumbers.append(number2)
-
-
-        yield {
-            "profile_url" : profile,
-            "first_name": first,
-            "middle_name": middle,
-            "last_name" : last,
-            "image_url": image,
-            "desciption": description,
-            "social": social_links,
-            "email": email,
-            "title": title,
-            "agent_phone_numbers": phonenumbers,
-            "office_phone_numbers": officenumbers,
+    #     phonenumbers = []
+    #     officenumbers = []
+    #     for i in numbers:
+    #         if ": " in i:
+    #             number = i.split(": ")[1]
+    #             phonenumbers.append(number)
+    #             if i.startswith("O: "):
+    #                 number2= i.split(": ")[1]
+    #                 officenumbers.append(number2)
 
 
+    #     yield {
+    #         "profile_url" : profile,
+    #         "first_name": first,
+    #         "middle_name": middle,
+    #         "last_name" : last,
+    #         "image_url": image,
+    #         "desciption": description,
+    #         "social": social_links,
+    #         "email": email,
+    #         "title": title,
+    #         "agent_phone_numbers": phonenumbers,
+    #         "office_phone_numbers": officenumbers,
 
-        }
+
+
+    #     }
 
 
 
